23617130880d53c45796d13951e2facbe4641baf
funhuman <51252171+funhuman@users.noreply.github.com>
2024-03-17 18:29:01 +0800

    build v0.14.0

--- README.md
+++
@@ -1,3 +0,0 @@
-## funhuman 的个人网站
-
-[点我前往](https://funhuman.github.io)
--- docs/js/web.load.js
+++
@@ -1,5 +0,0 @@
-web_list_html='';
-web_list.forEach(function (t) { web_list_html += `<div class="col-lg-3 col-md-4 col-xs-12"><div class="card"><a href="${t.href}" target="_blank"><div class="card-body"><h4 class="card-title">${t.title}</h4><p>${t.slogan}<span class="float-right">${t.kind_name}</span></p></a></div></div></div></div></div>` });
-$(".web-list").html(web_list_html);
-console.log("GitHub: https://github.com/funhuman/funhuman.github.io")
-console.log("Reference: https://gitee.com/xiangjianan/lks/")
