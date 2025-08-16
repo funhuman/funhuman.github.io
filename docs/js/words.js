4e65570bc2e4e500630f15a47cd0e31fd5c2b213
funhuman <51252171+funhuman@users.noreply.github.com>
2025-08-16 10:21:49 +0800

    v0.15.4
    add words

---
+++ docs/js/words.js
@@ -0,0 +1,11 @@
+const wordsData = [
+  { create_time: "2025-08-16", word: "非常喜欢一句话", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "一觉睡醒就这样了", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "让我看看怎么回事", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "您好，我仔细阅读了您发布的这个职位，和我自己比较匹配，希望能与您进一步沟通一下，您方便的话我给您发一份简历，期待您的回复", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "是他一个人这么干啊，还是大家都这么干啊", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "原来是这样", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "吃饭去啊", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "我要看一集上海堡垒缓一下", keywords: ["对话数据集"], title: "" },
+  { create_time: "2025-08-16", word: "等我问下副驾驶", keywords: ["对话数据集"], title: "" },
+];
